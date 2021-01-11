29ddcdc8eb6ac316d765fad2d2c809e9013adfa2
Alban JUTEAU <aljubrenetir@gmail.com>
2021-01-11 22:42:15 +0100

    initial commit

---
+++ src/main/java/com/albanj/capitalize/capitalizeback/CapitalizeBackApplication.java
@@ -0,0 +1,13 @@
+package com.albanj.capitalize.capitalizeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalizeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalizeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albanj/capitalize/capitalizeback/CapitalizeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albanj.capitalize.capitalizeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalizeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
